e670f99ee401f7d95a142ec61393d942dc3febd8
unknown <nathan.hlungwani123@gmail.com>
2024-07-02 18:55:38 +0200

    tried out if statements for the transport app

---
+++ main.js
@@ -0,0 +1,10 @@
+let pointA = prompt ('Enter First Destination (From) :');
+let pointB = prompt ('Enter Second Destination (To) :');
+
+if (pointA == "pretoria" && pointB == "johannesburg"){
+    alert(`Enter Destination`);
+}
+
+else {
+    alert (`your amount is R60`);
+}
